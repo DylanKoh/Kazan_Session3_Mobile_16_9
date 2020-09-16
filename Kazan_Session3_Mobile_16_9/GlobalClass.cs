7cc3e5de6b6817c3cd96da29efa787d55a9e19c1
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-16 16:33:41 +0800

    Almost completed Main Page functionality

---
+++ Kazan_Session3_Mobile_16_9/GlobalClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kazan_Session3_Mobile_16_9
+{
+    public class GlobalClass
+    {
+        public class Asset
+        {
+            public long ID { get; set; }
+            public string AssetSN { get; set; }
+            public string AssetName { get; set; }
+            public long DepartmentLocationID { get; set; }
+            public long EmployeeID { get; set; }
+            public long AssetGroupID { get; set; }
+            public string Description { get; set; }
+            public Nullable<System.DateTime> WarrantyDate { get; set; }
+        }
+
+        public partial class AssetOdometer
+        {
+            public long ID { get; set; }
+            public long AssetID { get; set; }
+            public System.DateTime ReadDate { get; set; }
+            public long OdometerAmount { get; set; }
+
+        }
+
+        public class PMScheduleModel
+        {
+            public long ID { get; set; }
+            public string Name { get; set; }
+            public Nullable<long> PMScheduleTypeID { get; set; }
+
+        }
+
+        public class PMScheduleType
+        {
+            public long ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class PMTask
+        {
+            public long ID { get; set; }
+            public long AssetID { get; set; }
+            public long TaskID { get; set; }
+            public long PMScheduleTypeID { get; set; }
+            public Nullable<System.DateTime> ScheduleDate { get; set; }
+            public Nullable<long> ScheduleKilometer { get; set; }
+            public Nullable<bool> TaskDone { get; set; }
+        }
+
+        public partial class Task1
+        {
+            public long ID { get; set; }
+            public string Name { get; set; }
+        }
+
+
+        public class CustomView
+        {
+            public int PMTaskID { get; set; }
+            public string Asset { get; set; }
+            public string TaskName { get; set; }
+            public string TaskTypeAndValue { get; set; }
+            public bool TaskDone { get; set; }
+        }
+
+    }
+}
--- Kazan_Session3_Mobile_16_9/MainPage.xaml.cs
+++ Kazan_Session3_Mobile_16_9/MainPage.xaml.cs
@@ -1,23 +1,77 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static Kazan_Session3_Mobile_16_9.GlobalClass;
 
 namespace Kazan_Session3_Mobile_16_9
 {
     public partial class MainPage : ContentPage
     {
+        List<Asset> _assetList;
+        List<Task1> _taskList;
+        List<CustomView> _customList = new List<CustomView>();
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void dpActive_DateSelected(object sender, DateChangedEventArgs e)
+        protected async override void OnAppearing()
         {
+            base.OnAppearing();
+            await LoadPickers();
+            await LoadData(dpActive.Date);
+        }
 
+        private async Task LoadData(DateTime activeDate)
+        {
+            lvPMTask.ItemsSource = null;
+            _customList.Clear();
+            var client = new WebApi();
+            var runResponse = await client.PostAsync(null, "PMTasks/GetRunTaskNotDone");
+            _customList = JsonConvert.DeserializeObject<List<CustomView>>(runResponse);
+            var timeOverResponse = await client.PostAsync(null, $"PMTasks/GetOverTime?activeDate={activeDate}");
+            _customList.AddRange(JsonConvert.DeserializeObject<List<CustomView>>(timeOverResponse));
+            var timeCurrentResponse = await client.PostAsync(null, $"PMTasks/GetCurrentTime?activeDate={activeDate}");
+            _customList.AddRange(JsonConvert.DeserializeObject<List<CustomView>>(timeCurrentResponse));
+            var timeAboutResponse = await client.PostAsync(null, $"PMTasks/GetAboutTime?activeDate={activeDate}");
+            _customList.AddRange(JsonConvert.DeserializeObject<List<CustomView>>(timeAboutResponse));
+            var runDoneResponse = await client.PostAsync(null, $"PMTasks/GetRunTaskDone");
+            _customList.AddRange(JsonConvert.DeserializeObject<List<CustomView>>(runDoneResponse));
+            var timeDoneResponse = await client.PostAsync(null, $"PMTasks/GetTimeDone");
+            _customList.AddRange(JsonConvert.DeserializeObject<List<CustomView>>(timeDoneResponse));
+            lvPMTask.ItemsSource = _customList;
+        }
+
+        private async Task LoadPickers()
+        {
+            pAssetName.Items.Clear();
+            pTask.Items.Clear();
+            var client = new WebApi();
+            var assetResponse = await client.PostAsync(null, "Assets");
+            _assetList = JsonConvert.DeserializeObject<List<Asset>>(assetResponse);
+            foreach (var item in _assetList)
+            {
+                pAssetName.Items.Add(item.AssetName);
+            }
+
+            var taskResponse = await client.PostAsync(null, "Tasks");
+            _taskList = JsonConvert.DeserializeObject<List<Task1>>(taskResponse);
+            foreach (var item in _taskList)
+            {
+                pTask.Items.Add(item.Name);
+            }
+
+        }
+
+        private async void dpActive_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            await LoadData(dpActive.Date);
         }
 
         private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
@@ -25,19 +79,58 @@ namespace Kazan_Session3_Mobile_16_9
 
         }
 
-        private void pAssetName_SelectedIndexChanged(object sender, EventArgs e)
+        private async void pAssetName_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (pAssetName.SelectedItem != null)
+            {
+                FilterList();
+            }
+            else
+            {
+                await LoadData(dpActive.Date);
+            }
         }
 
-        private void pTask_SelectedIndexChanged(object sender, EventArgs e)
+        private void FilterList()
         {
-
+            if (pAssetName.SelectedItem != null && pTask.SelectedItem != null)
+            {
+                var filteredList = (from x in _customList
+                                    where x.Asset.Contains(pAssetName.SelectedItem.ToString()) && x.TaskName.Contains(pTask.SelectedItem.ToString())
+                                    select x).ToList();
+                lvPMTask.ItemsSource = filteredList;
+            }
+            else if (pAssetName.SelectedItem == null && pTask.SelectedItem != null)
+            {
+                var filteredList = (from x in _customList
+                                    where x.TaskName.Contains(pTask.SelectedItem.ToString())
+                                    select x).ToList();
+                lvPMTask.ItemsSource = filteredList;
+            }
+            else if (pAssetName.SelectedItem != null && pTask.SelectedItem == null)
+            {
+                var filteredList = (from x in _customList
+                                    where x.Asset.Contains(pAssetName.SelectedItem.ToString())
+                                    select x).ToList();
+                lvPMTask.ItemsSource = filteredList;
+            }
         }
 
-        private void btnClear_Clicked(object sender, EventArgs e)
+        private async void pTask_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (pTask.SelectedItem != null)
+            {
+                FilterList();
+            }
+            else
+            {
+                await LoadData(dpActive.Date);
+            }
+        }
 
+        private async void btnClear_Clicked(object sender, EventArgs e)
+        {
+            await LoadPickers();
         }
 
         private void btnAdd_Clicked(object sender, EventArgs e)
