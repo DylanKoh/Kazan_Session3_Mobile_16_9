eb7bb454c73b56cde1621d065e5c16d4492f00ad
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-16 14:00:12 +0800

    Completed Web Api class to do posting

---
+++ Kazan_Session3_Mobile_16_9/WebApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kazan_Session3_Mobile_16_9
+{
+    public class WebApi
+    {
+        string baseAddress = "http://10.0.2.2:55423/";
+
+        public async Task<string> PostAsync(string data, string extSite)
+        {
+            var client = new HttpClient();
+            var response = "";
+            var site = baseAddress + extSite;
+            if (data == null)
+            {
+                var emptyContent = new StringContent("", Encoding.UTF8, "application/json");
+                response = await client.PostAsync(site, emptyContent).Result.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+                response = await client.PostAsync(site, content).Result.Content.ReadAsStringAsync();
+            }
+            return response;
+        }
+    }
+}
