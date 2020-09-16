edd31906383d52e933066cf3207410dc706662ef
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-16 11:37:31 +0800

    Add project files.

---
+++ Kazan_Session3_Mobile_16_9/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kazan_Session3_Mobile_16_9
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
